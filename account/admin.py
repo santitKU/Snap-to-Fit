90c647afcd4ff5652c7947008eeee52176d3b8cb
santitKU <ss26121321@student.ku,edu.np>
2022-11-11 15:16:23 +0545

    Login and sign up pages added

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from account.models import Doctor, Patient, User
 # Register your models here.
+admin.site.register(Patient)
+admin.site.register(User)
+admin.site.register(Doctor)
--- manage.py
+++ manage.py
@@ -20,5 +20,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# hello this is test branch
