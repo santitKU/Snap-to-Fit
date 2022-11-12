1cd8fd928ad28365f1ca2748b5d127aae833584d
santitKU <ss26121321@student.ku,edu.np>
2022-11-12 20:41:07 +0545

    database added and login form edited

--- base/models.py
+++ base/models.py
@@ -3,32 +3,42 @@ from django.db import models
 from django.contrib.auth.models import User
 from account.models import Patient
 from django.urls import reverse
-
+# class SleepTable(models.Model):
+#     deepSleepTime = models.IntegerField(blank=False, null=False)
+#     shallowSleepTime = models.IntegerField(blank=False, null=False)
+#     wakeTime = models.IntegerField(blank=False, null=False)
+#     start = models.IntegerField(blank=False, null=False)
+#     stop = models.IntegerField(blank=False, null=False)
 # Create your models here.
 class DailyVitals(models.Model):
     patient = models.ForeignKey(Patient, on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField(auto_now_add=True)
-    blood_pressure = models.IntegerField(blank = True, null=True)
-    sugar_level = models.IntegerField(blank = True, null=True)
-    temperature = models.IntegerField(blank = True, null=True)
-    weight = models.IntegerField(blank = True, null=True)
-    age = models.IntegerField(blank = True, null=True)
-    GENDER_CHOICES = (
-        ('M', 'Male'),
-        ('F', 'Female'),
-    )
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)#either 1 or 0
-    height = models.IntegerField(blank = True, null=True)
-    bodyfat = models.IntegerField(blank = True, null=True)
-    diastolic = models.IntegerField(blank = True, null=True)
-    systolic = models.IntegerField(blank = True, null=True)
-    gripForce = models.IntegerField(blank = True, null=True)
-    situps_count = models.IntegerField(blank = True, null=True)
-    sit_bend_forward = models.IntegerField(blank = True, null=True)
-    broadjump_cm = models.IntegerField(blank = True, null=True)
-    outcome = models.IntegerField(blank = True, null=True)
-    bmi = models.IntegerField(blank = True, null=True)
-    pregnancy = models.IntegerField(blank = True, null=True)
+    heart_rate = models.IntegerField(blank=True, null=True)
+    calories = models.FloatField(blank=True, null=True)
+    steps = models.IntegerField(blank=True, null=True)
+    distance = models.FloatField(blank=True, null=True)
+    # sleeptime = models.OneToOneField(SleepTable, on_delete=CASCADE)
+    # blood_pressure = models.IntegerField(blank = True, null=True)
+    # sugar_level = models.IntegerField(blank = True, null=True)
+    # temperature = models.IntegerField(blank = True, null=True)
+    # weight = models.IntegerField(blank = True, null=True)
+    # age = models.IntegerField(blank = True, null=True)
+    # GENDER_CHOICES = (
+    #     ('M', 'Male'),
+    #     ('F', 'Female'),
+    # )
+    # gender = models.CharField(max_length=1, choices=GENDER_CHOICES)#either 1 or 0
+    # height = models.IntegerField(blank = True, null=True)
+    # bodyfat = models.IntegerField(blank = True, null=True)
+    # diastolic = models.IntegerField(blank = True, null=True)
+    # systolic = models.IntegerField(blank = True, null=True)
+    # gripForce = models.IntegerField(blank = True, null=True)
+    # situps_count = models.IntegerField(blank = True, null=True)
+    # sit_bend_forward = models.IntegerField(blank = True, null=True)
+    # broadjump_cm = models.IntegerField(blank = True, null=True)
+    # outcome = models.IntegerField(blank = True, null=True)
+    # bmi = models.IntegerField(blank = True, null=True)
+    # pregnancy = models.IntegerField(blank = True, null=True)
     
 
     def __str__(self):
